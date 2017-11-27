0d0ab1f44ff194f6561e2fb6e08efafec535bead
nezumi <nezumi@localhost.localdomain>
2017-11-27 22:32:40 +0100

    Obecny projekt

---
+++ src/main/java/domain/Actor.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Actor {
+    private int id;
+    private String name, surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
---
+++ src/main/java/domain/services/ActorService.java
@@ -0,0 +1,50 @@
+package domain.services;
+
+import domain.Actor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActorService {
+    private static ActorService instance = null;
+    private static List<Actor> db = new ArrayList<>();
+    private static int currentID = 1;
+
+    protected ActorService() {
+    }
+
+    public static ActorService getInstance() {
+        if (instance == null) {
+            instance = new ActorService();
+        }
+        return instance;
+    }
+
+    public Actor get(int id) {
+        for (Actor a : db)
+            if (a.getId() == id)
+                return a;
+        return null;
+    }
+
+    public void add(Actor a) {
+        a.setId(++currentID);
+        db.add(a);
+    }
+
+    public void update(Actor actor) {
+        for (Actor a : db)
+            if (a.getId() == actor.getId()) {
+                a.setName(actor.getName());
+                a.setSurname(actor.getSurname());
+            }
+    }
+
+    public void delete(Actor a) {
+        db.remove(a);
+    }
+
+    public List<Actor> getAll() {
+        return db;
+    }
+}
