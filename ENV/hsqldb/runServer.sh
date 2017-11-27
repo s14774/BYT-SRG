6ff45c48127f6da5bda9d3c76b5d3b0eb6c82b6e
nezumi <nezumi@localhost.localdomain>
2017-11-27 23:59:27 +0100

    ENv

---
+++ ENV/hsqldb/runServer.sh
@@ -0,0 +1,6 @@
+
+#cd ..\data
+#@java -classpath ../lib/hsqldb.jar org.hsqldb.server.Server %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+cd data
+java -classpath ../lib/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
